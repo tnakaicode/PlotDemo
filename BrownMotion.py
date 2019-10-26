44bbd6fae2bf270ea3793546b256ab47c62a460d
Taku Nakai <t.nakai.office@gmail.com>
2019-10-27 04:13:08 +0900

    elipsoid pnt

--- BrownMotion.py
+++ BrownMotion.py
@@ -9,27 +9,65 @@ from sys import exit
 
 from OCC.gp import gp_Pnt, gp_Vec, gp_Dir
 from OCC.gp import gp_Ax1, gp_Ax2, gp_Ax3
+from OCC.gp import gp_Lin
+from OCC.Geom import Geom_Line, Geom_Surface
+from OCC.BRep import BRep_Tool_Surface
+from OCC.GeomAPI import GeomAPI_IntCS
+from OCCUtils.Topology import Topo
 
 from base import plot2d, plot3d, plotocc
+from base import gen_ellipsoid, set_trf
 
 
 class BrownMotion (object):
 
-    def __init__(self):
+    def __init__(self, axs=gp_Ax3()):
         self.dm = 3
         self.nx = 100
         self.px = np.zeros([self.dm, self.nx])
         self.pt = np.linspace(0, 1, self.nx)
-        self.rxyz = [10., 15., 20.]
+        self.rxyz = [10., 11., 9.]
         self.r_min = np.min(self.rxyz)
         self.r_max = np.max(self.rxyz)
 
+        self.axis = axs
+        self.trsf = set_trf(ax2=self.axis)
+        self.ellp = gen_ellipsoid(axs=self.axis, rxyz=self.rxyz)
+        self.get_surf()
+
+        lin = Geom_Line(self.axis.Location(), gp_Dir(0, 0, 1))
+        self.int_surf = GeomAPI_IntCS(lin.GetHandle(), self.ellp_surf)
+
+        p0 = self.rand_pnt()
+        p1 = self.rand_pnt()
+        self.get_pnt_ellipsoid(p0)
+
+    def get_surf(self):
+        top_api = Topo(self.ellp)
+        print(top_api.number_of_faces())
+        for face in top_api.faces():
+            ellp_face = face
+        self.ellp_surf = BRep_Tool_Surface(ellp_face)
+
     def rand_point(self):
-        dx = np.random.randn(self.dm)
+        dx = np.random.uniform(-1, 1, self.dm)
         dx_n = np.sqrt(np.sum(dx ** 2))
         x, y, z = dx / dx_n
         p, q, r = self.rxyz
         return x * p, y * q, z * r
+
+    def rand_pnt(self):
+        dx = np.random.randn(self.dm)
+        dx_n = np.sqrt(np.sum(dx ** 2))
+        return gp_Pnt(*dx / dx_n)
+
+    def get_pnt_ellipsoid(self, pnt=gp_Pnt(0, 0, 1)):
+        vec = gp_Vec(gp_Pnt(), pnt)
+        h_lin = Geom_Line(gp_Ax1(gp_Pnt(), gp_Dir(vec))).GetHandle()
+        self.int_surf.Perform(h_lin, self.ellp_surf)
+        print(pnt)
+        print(self.int_surf.Parameters(1))
+        print(self.int_surf.Parameters(2))
 
     def plot_plt3d(self):
         obj = plot3d()
@@ -48,15 +86,15 @@ class BrownMotion (object):
         obj.show_pnt([self.rxyz[0], 0, 0])
         obj.show_pnt([0, self.rxyz[1], 0])
         obj.show_pnt([0, 0, self.rxyz[2]])
+        obj.display.DisplayShape(self.ellp, transparency=0.5, color="BLUE")
         print(*self.rand_point())
         obj.show_pnt(self.rand_point())
         obj.show_pnt(self.rand_point())
         obj.show_pnt(self.rand_point())
         obj.show_pnt(self.rand_point())
-        obj.show_ellipsoid(rxyz=self.rxyz)
-        obj.show_ball(scale=self.rxyz[0], trans=0.9)
-        obj.show_ball(scale=self.rxyz[1], trans=0.8)
-        obj.show_ball(scale=self.rxyz[2], trans=0.7)
+        #obj.show_ball(scale=self.rxyz[0], trans=0.9)
+        #obj.show_ball(scale=self.rxyz[1], trans=0.8)
+        #obj.show_ball(scale=self.rxyz[2], trans=0.7)
         obj.show_axs_pln(scale=self.r_min)
         obj.show()
 
