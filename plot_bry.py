87125d6f5b7305cba3ce64009ac7363c01a8f26e
Taku Nakai <t.nakai.office@gmail.com>
2019-10-30 06:47:57 +0900

    btr

--- plot_bry.py
+++ plot_bry.py
@@ -34,16 +34,18 @@ class GenArea (object):
 
 class GenBoundary (plot2d):
 
-    def __init__(self):
+    def __init__(self, num=5, lxy=10):
         plot2d.__init__(self)
         self.xx = []
         self.yy = []
         self.kk = 3
-        self.pn = 10
+        self.pn = num
 
-        self.axs.plot([-10, 10, 10, -10, -10], [-10, -10, 10, 10, -10])
-        self.spl, = self.axs.plot([], [])
-        
+        self.axs.plot([-lxy, lxy, lxy, -lxy, -lxy],
+                      [-lxy, -lxy, lxy, lxy, -lxy])
+        self.spl, = self.axs.plot(self.xx, self.yy)
+        self.pts, = self.axs.plot(self.xx, self.yy, 'o')
+
         self.fig.canvas.mpl_connect('button_press_event', self._OnClick)
         self.fig.canvas.mpl_connect('key_press_event', self._KeyClick)
 
@@ -51,23 +53,30 @@ class GenBoundary (plot2d):
         pos_axs = self.fig.add_axes(pos)
         axs_bot = Button(pos_axs, name)
         return axs_bot
-    
+
     def _KeyClick(self, event):
         if event.key == 'p':
             self._Spline(event)
-        else:
+        elif event.key == 'c':
             self._Clear(event)
 
     def _Clear(self, event):
         self.xx = []
         self.yy = []
-        # plt.draw()
+        self.pn = 2
+        self.spl.set_xdata(self.xx)
+        self.spl.set_ydata(self.yy)
+        self.pts.set_xdata(self.xx)
+        self.pts.set_ydata(self.yy)
+        plt.draw()
 
     def _Spline(self, event):
+        num = len(self.xx)
         tck, u = interpolate.splprep([self.xx, self.yy], k=self.kk, s=0)
-        pu = np.linspace(0, 1, num=self.pn, endpoint=True)
+        pu = np.linspace(0, 1, num=self.pn * num)
         spline = interpolate.splev(pu, tck)
-        self.spl.set_data(spline)
+        self.spl.set_xdata(spline[0])
+        self.spl.set_ydata(spline[1])
         plt.draw()
 
     def _OnClick(self, event):
@@ -77,6 +86,9 @@ class GenBoundary (plot2d):
             print(event)
             self.xx.append(event.xdata)
             self.yy.append(event.ydata)
+            self.pts.set_xdata(self.xx)
+            self.pts.set_ydata(self.yy)
+            plt.draw()
 
 
 if __name__ == '__main__':
