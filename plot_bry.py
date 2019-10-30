173831a932df805cb864391fc438edae3cd916c5
Taku Nakai <t.nakai.office@gmail.com>
2019-10-31 04:07:37 +0900

    convex plot

--- plot_bry.py
+++ plot_bry.py
@@ -6,32 +6,6 @@ from scipy import interpolate
 from base import plot2d
 
 
-class GenArea (object):
-
-    def __init__(self):
-        plot2d.__init__(self)
-        self.pnt = []
-        self.xy0 = np.array([0, 0])
-        self.pnt.append(self.xy0)
-
-        #self.fig, self.axs = plt.subplots()
-        # self.axs.set_aspect('equal')
-        # self.axs.xaxis.grid()
-        # self.axs.yaxis.grid()
-
-    def gen_arc(self, sxy=[0, 0], radi=10, deg=[-30, 60], num=50):
-        pd = np.linspace(*deg, num)
-        pr = np.deg2rad(pd)
-        for i, v in enumerate(pr):
-            x = sxy[0] + radi * np.cos(v)
-            y = sxy[0] + radi * np.sin(v)
-            self.pnt.append(np.array([x, y]))
-
-    def data_ouput(self):
-        dat = np.array(obj.pnt)
-        np.savetxt("./tmp/data_area.txt", dat)
-
-
 class GenBoundary (plot2d):
 
     def __init__(self, num=5, lxy=10):
