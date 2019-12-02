456ba80ab27f03db78e0dfd9911d811f1d026963
Taku Nakai <tnakaicode@gmail.com>
2019-12-02 17:26:36 +0900

    FaireCurve explain

--- FairCurve_simple.py
+++ FairCurve_simple.py
@@ -29,8 +29,41 @@ from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakeEdge
 from OCC.Core.FEmTool import FEmTool_ProfileMatrix
 from OCC.Display.SimpleGui import init_display
 
-
 from base import plotocc
+
+# FairCurve
+#
+# Constructs the two contact points P1 and P2
+# The geometrical characteristics of the batten (elastic beam)
+#
+# These include the real number values for
+#   height of deformation Height,
+#   slope value Slope
+#   kind of energy PhysicalRatio.
+#       Jerk (0)
+#       Sagging (1).
+#
+# Note that the default setting for Physical Ration are initialized
+#   FreeSliding = False
+#   ConstraintOrder1 = 1
+#   ConstraintOrder2 = 1
+#   Angle1 = 0
+#   Angle2 = 0
+#   Curvature1 = 0
+#   Curvature2 = 0
+#   SlidingFactor = 1
+#
+# Warning If PhysicalRatio equals 1,
+#   you cannot impose constraints on curvature.
+#
+# Exceptions NegativeValue if Height is less than or equal to 0.
+#
+# NullValue if the distance between P1 and P2 is less than or equal to
+#   the tolerance value for distance in Precision::Confusion: P1.IsEqual(P2, Precision::Confusion()).
+#
+# The function gp_Pnt2d::IsEqual tests to see if this is the case.
+#
+# Definition of the geometricals constraints
 
 
 class DrawCurve (plotocc):
@@ -41,6 +74,7 @@ class DrawCurve (plotocc):
         self.pt2 = gp_Pnt2d(0., 120.)
         self.hit = 100.
         self.pln = Geom_Plane(gp_Pln())
+
         self.fc_MinVar = FairCurve_MinimalVariation(
             self.pt1, self.pt2, self.hit, 1)
         self.fc_MinVar.SetP1(self.pt1)
@@ -52,35 +86,27 @@ class DrawCurve (plotocc):
         self.fc_MinVar.SetHeight(self.hit)
         self.fc_MinVar.SetSlope(0)
         self.fc_MinVar.SetFreeSliding(True)
+        self.fc_MinVar.SetSlidingFactor(0.1)
+        self.fc_MinVar.SetPhysicalRatio(0.5)
         stat = self.fc_MinVar.Compute()
 
-        for i in range(0, 40):
+        for i in range(0, 20):
             # TODO: the parameter slope needs to be visualized
-            slope = i / 100.
+            slope = i / 20.
             self.fc_MinVar.SetAngle1(np.deg2rad(i))
             self.fc_MinVar.SetAngle2(np.deg2rad(-i))
             self.fc_MinVar.SetSlope(slope)
-            print(self.fc_MinVar.DumpToString())
+            txt = "\r {}".format(self.fc_MinVar.DumpToString())
+            # print(self.fc_MinVar.DumpToString())
             self.fc_MinVar.Compute()
-            self.display.EraseAll()
-            edge = BRepBuilderAPI_MakeEdge(
-                self.fc_MinVar.Curve(), self.pln).Edge()
-            self.display.DisplayShape(edge, update=True)
+            # self.display.EraseAll()
+            self.display.DisplayShape(self.fc_MinVar.Curve(), update=True)
+            sys.stdout.write(txt)
+            sys.stdout.flush()
+            print()
             # time.sleep(0.21)
-
-    def batten_curve(self, slope, angle1, angle2):
-        fc = FairCurve_MinimalVariation(self.pt1, self.pt2, self.hit, slope)
-        fc.SetConstraintOrder1(2)
-        fc.SetConstraintOrder2(2)
-        fc.SetAngle1(angle1)
-        fc.SetAngle2(angle2)
-        fc.SetHeight(self.hit)
-        fc.SetSlope(slope)
-        fc.SetFreeSliding(True)
-        print(fc.DumpToString())
-        status = fc.Compute()
-        print(error_code(status[0]), error_code(status[1]))
-        return fc.Curve()
+        self.display.DisplayShape(
+            self.fc_MinVar.Curve(), update=True, color="BLUE")
 
 
 def error_code(n):
