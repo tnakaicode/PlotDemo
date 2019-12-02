a1e9efecfe3903b54aefaac4ddfcde61da8024e7
Taku Nakai <tnakaicode@gmail.com>
2019-12-02 13:10:49 +0900

    pipe

--- GenPipe.py
+++ GenPipe.py
@@ -9,14 +9,21 @@ class PipePlot(plot2d):
 
     def __init__(self):
         plot2d.__init__(self)
-        px = np.linspace(0, 1, 100)
-        self.axs.plot(px, self.logistic_func(px, lxy=[10.0, 15.0]))
-        self.axs.plot(px, self.logistic_func(px, lxy=[10.0, 9.0], x0=0.1))
+        px = np.linspace(0, 10, 100)
+        self.axs.plot(px, self.logistic_func(
+            px, l0=10.0, rx=[2.5, 7.5], lx=[11.0, 9.0], gx=[5.0, 20.0]
+        ))
         self.axs.set_aspect('auto')
-        self.axs.set_ylim(0, 20)
 
-    def logistic_func(self, px, x0=0.0, lxy=[10, 15.0], grow=1.0):
-        val = (lxy[1] - lxy[0]) / (1 + np.exp(-grow * (px - x0))) + lxy[0]
+    def logistic_func(self, px, l0=10.0, lx=[11.0], rx=[0.1], gx=[0.1]):
+        val = l0
+        for i, v in enumerate(lx):
+            idx = i + 1
+            if i == 0:
+                val += (lx[i] - l0) / (1 + np.exp(-gx[i] * (px - rx[i])))
+            else:
+                val += (lx[i] - lx[i - 1]) / \
+                    (1 + np.exp(-gx[i] * (px - rx[i])))
         return val
 
 
